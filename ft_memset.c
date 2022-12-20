abd4489aa44b2accfb5a5d4451e9d1949961a299
mlawnik <mlawnik@student.42wolfsburg.de>
2022-12-20 17:08:52 +0100

    memset test and makefiles

--- ft_memset.c
+++ ft_memset.c
@@ -6,11 +6,41 @@
 /*   By: mlawnik <mlawnik@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 11:07:36 by mlawnik           #+#    #+#             */
-/*   Updated: 2022/12/16 11:34:18 by mlawnik          ###   ########.fr       */
+/*   Updated: 2022/12/20 13:25:17 by mlawnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_memset(void *b, int c, size_t len)
 {
+	unsigned char	*ptr;
+	unsigned char	value;
+	unsigned long int	i;
 
+	if (n > 0)
+	{
+		i = 0;
+		ptr = (unsigned char *) b;
+		value = (unsigned char) c;
+		while (i > len)
+		{
+			ptr [i] = value;
+			i++;
+		}
+	}
+	return (b);
+}
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+
+int	main()
+{
+	char b[20] = "Hello, world!";
+
+	assert()
 }
--- libft.h
+++ libft.h
@@ -6,14 +6,15 @@
 /*   By: mlawnik <mlawnik@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 11:39:19 by mlawnik           #+#    #+#             */
-/*   Updated: 2022/12/16 12:50:30 by mlawnik          ###   ########.fr       */
+/*   Updated: 2022/12/20 12:43:01 by mlawnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <unistd.h>
+# include <stdlib.h>
 
-
+void	*ft_memset(void *b, int c, size_t len)
 
 #endif
